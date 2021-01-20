89c5e56609079a6bb73844e0d43c2876d65476f0
Cristian Suarez <cdsp10@hotmail.com>
2021-01-20 12:15:42 -0500

    variable de entrono mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
     
 } else {
-    urlDB = 'mongodb://root:root@cluster0-shard-00-00.75w5g.mongodb.net:27017,cluster0-shard-00-01.75w5g.mongodb.net:27017,cluster0-shard-00-02.75w5g.mongodb.net:27017/cafe?replicaSet=atlas-11n289-shard-0&ssl=true&authSource=admin';
+    urlDB =process.env.MONGO_URI;
 }
 
 
